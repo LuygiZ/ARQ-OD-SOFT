a0afc6a92f7ba50f4860d53206c241c5343a9a20
Luigi <Lmname999@gmail.com>
2025-12-28 04:06:47 +0000

    Refactored original files from pt1 and started implementing author microservice

---
+++ author-service/src/test/java/pt/psoft/author/AuhtorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.psoft.author;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuhtorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
