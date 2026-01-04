8be8733976c2504a6210419d111128599fac351c
Henrique <henrique@example.com>
2026-01-04 13:08:30 +0000

    Fix compilation error: Remove ProviderType.ASYNC usage

--- user-service/src/test/java/pt/psoft/user/messaging/ReaderCreatedConsumerTest.java
+++ user-service/src/test/java/pt/psoft/user/messaging/ReaderCreatedConsumerTest.java
@@ -4,7 +4,6 @@ import au.com.dius.pact.consumer.MessagePactBuilder;
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
-import au.com.dius.pact.consumer.junit5.ProviderType;
 import au.com.dius.pact.core.model.annotations.Pact;
 import au.com.dius.pact.core.model.messaging.Message;
 import au.com.dius.pact.core.model.messaging.MessagePact;
